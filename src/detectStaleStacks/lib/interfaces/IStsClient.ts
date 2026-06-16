f50a555f63989a19c8e18bcd64b8c21d9a48b080
phillarmstrong-GDS <phill.armstrong@digital.cabinet-office.gov.uk>
2026-06-16 16:14:36 +0100

    feat(PLAT-477): tidy

--- src/detectStaleStacks/lib/interfaces/IStsClient.ts
+++ src/detectStaleStacks/lib/interfaces/IStsClient.ts
@@ -1,5 +1,10 @@
 import { Credentials, AssumeRoleCommand } from '@aws-sdk/client-sts';
 
+interface response {
+  Credentials: Credentials;
+  $metadata?: any;
+}
+
 export interface IStsClient {
-  send(command: AssumeRoleCommand): Promise<Credentials>;
+  send(command: AssumeRoleCommand): Promise<response>;
 }
