2261120e094e02642928eeef01d207aefb1471c9
phillarmstrong-GDS <phill.armstrong@digital.cabinet-office.gov.uk>
2026-07-03 12:44:02 +0100

    feat(PLAT-478): dlete stack lambda

--- src/detectStaleStacks/index.ts
+++ src/detectStaleStacks/index.ts
@@ -3,7 +3,7 @@ import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 
 import { appVariables } from '../shared/appConfig';
 import { QueueMessage } from '../shared/queueMessage';
-import { Processor } from './Processor';
+import { Processor } from './processor';
 
 export const handler = async () => {
   console.log('Seeking stale stacks');
