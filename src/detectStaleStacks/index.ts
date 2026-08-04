9aa744f46866cc442a2bdbf5f89fa16645db8a31
darkynt <1789097+darkynt@users.noreply.github.com>
2026-08-04 12:17:11 +0100

    cleanup: remove inline spike/test code

--- src/detectStaleStacks/index.ts
+++ src/detectStaleStacks/index.ts
@@ -1,27 +1,11 @@
 import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
-import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
-
 import { appVariables } from '../shared/appConfig';
-import { QueueMessage } from '../shared/queueMessage';
 import { Processor } from './Processor';
 
 export const handler = async () => {
   console.log('Seeking stale stacks');
 
   const sns = new SNSClient({});
-  const sqsClient = new SQSClient({});
-
-  const message: QueueMessage = {
-    correlationId: crypto.randomUUID(),
-    batchId: crypto.randomUUID(),
-    accountId: '12345678',
-    accountName: 'testing',
-    region: 'eu-west2',
-    stackName: 'old stack',
-    deleteOrder: 1,
-    reason: 'can I send',
-    lastTouched: `${Date.now()}`,
-  };
 
   try {
     const processor = new Processor();
@@ -29,23 +13,6 @@ export const handler = async () => {
   } catch (error) {
     console.error(
       `Failed to run processor ${JSON.stringify({
-        error,
-      })}`,
-    );
-  }
-
-  try {
-    await sqsClient.send(
-      new SendMessageCommand({
-        QueueUrl: appVariables.QUEUE_URL,
-        MessageBody: JSON.stringify(message),
-      }),
-    );
-  } catch (error) {
-    console.error(
-      `Failed to publish message ${JSON.stringify({
-        queueUrl: appVariables.QUEUE_URL,
-        message: JSON.stringify(message),
         error,
       })}`,
     );
@@ -77,8 +44,7 @@ export const handler = async () => {
     },
     body: JSON.stringify({
       message:
-        'And is a goodnight from him, the time is ' +
-        new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' }),
-    }),
+        'OK'
+      }),
   };
 };
